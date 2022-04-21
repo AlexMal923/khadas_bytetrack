2cde2ed5c37330859757c90d2374e43b2d44f346
Alexander <84590713+SashaAlderson@users.noreply.github.com>
2022-04-21 12:04:07 +0700

    Change to stable version

--- yolov5.py
+++ yolov5.py
@@ -1,98 +1,63 @@
-import ctypes 
+from ctypes import *
 import cv2
-import time
+import time 
 import copy
-import threading
-import multiprocessing as mp
 import numpy as np
-libc = ctypes.cdll.LoadLibrary("./yolov5_lib.so")
-from multiprocessing import Process
-
-libc.set_image_wrapper.argtypes = [ctypes.c_void_p, ctypes.c_int, ctypes.c_int, ctypes.c_void_p, ctypes.c_int, ctypes.c_int]
-libc.postpress_graph_image_wrapper.argtypes = [ctypes.c_void_p, ctypes.c_int, ctypes.c_int, ctypes.c_void_p, ctypes.c_void_p,
-                                                ctypes.c_int , ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_int]
+import threading
+libc = cdll.LoadLibrary("./yolov5_lib.so")
+libc.set_image_wrapper.argtypes = [c_void_p, c_int, c_int, c_void_p, c_int, c_int]
+libc.postpress_graph_image_wrapper.argtypes = [c_void_p, c_int, c_int, c_void_p, c_void_p,
+                                                c_int , c_int, c_int, c_int, c_int, c_int]
 class YOLOV5(threading.Thread):
     def __init__(self, model):
         threading.Thread.__init__(self)
-        self.init = False
-        self.cap = cv2.VideoCapture(0)
-        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 352)
-        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 288)
-        self.imsize = 352
         self.context = libc.create_context("timvx".encode('utf-8'), 1)
-        if libc.init_tengine():
-            print("Tengine not initialised")
-        print("Setting context device")
+        libc.init_tengine()
         rtt = libc.set_context_device(self.context, "TIMVX".encode('utf-8'), None, 0)
-        print("Rtt:", rtt)
-        print("Creating graph")
-        self.model_file = model
-        self.graph = libc.create_graph(self.context, "tengine".encode('utf-8'), self.model_file.encode('utf-8'))
-        print("Setting graph: ", self.graph)
-        libc.set_graph(self.imsize, self.imsize, self.graph)
-        print("Getting input tensor")
+        model_file = model.encode('utf-8')
+        self.graph = libc.create_graph(self.context, "tengine".encode('utf-8'), model_file)
+        libc.set_graph(352, 352, self.graph)
         self.input_tensor = libc.get_graph_input_tensor(self.graph, 0, 0)
-        print("Getting output node num")
         self.output_node_num = libc.get_graph_output_node_number(self.graph)
-        print("Initialised!")
-        start = time.time()
-
-        self.frame = None
+        self.cap = cv2.VideoCapture(0)
+        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+        self.cap.set(cv2.CAP_PROP_FPS, 30)
         self.dets = np.zeros([30,6], dtype = np.float32)
-        self.init = True
-        self.inferenced = False
-
+        self.last_frame = None
+        self.last_dets = None
+        print("Initialised")
     def inference(self):
-        start = time.time()
-        self.ret, self.frame = self.cap.read()
-        print("Ret: ", self.ret)
-        self.frame = self.frame[:,:, ::-1]        
-        libc.set_image_wrapper(self.frame.ctypes.data, 288, 352, self.input_tensor, self.imsize, self.imsize)
-        print("Set images for  ", time.time() - start)
-        start = time.time()
-        libc.run_graph(self.graph, 1)
-        print("Graph ran for ", time.time() - start)
-        start = time.time()
-        self.last_frame = copy.deepcopy(self.frame[:,:, ::-1])
-        print("Copied picture for ", time.time() - start)
-        self.inferenced = True
+        while True:
+            start = time.time()        
+            # image = "dog.jpg"
+            # frame = cv2.imread(image)
+            ret, self.frame = self.cap.read()
+            print("reading from camera for: ", time.time() - start)
+            height, width, _ = self.frame.shape
+            self.frame = self.frame[:,:,::-1]
+            libc.set_image_wrapper(self.frame.ctypes.data, height, width, self.input_tensor, 352, 352)
+            print("Set images for  ", time.time() - start)
+            start = time.time()
+            libc.run_graph(self.graph, 1)
+            print("Graph ran for ", time.time() - start)
+            start = time.time()
 
-    def post(self):
-        while self.inferenced:
-            start = time.time()            
-            libc.postpress_graph_image_wrapper(self.last_frame.ctypes.data, 288, 352, self.dets.ctypes.data, self.graph,self.output_node_num, self.imsize, self.imsize,3,9,0)
+            libc.postpress_graph_image_wrapper(self.frame.ctypes.data, height, width, self.dets.ctypes.data, self.graph,self.output_node_num, 352,352,3,9,0)
             print("Postprocess ran for ", time.time() - start)
-            self.inferenced = False
+            self.last_frame = copy.deepcopy(self.frame[::,::,::-1])
+            self.last_dets = copy.deepcopy(self.dets)
             cv2.imshow('frame', self.last_frame)
             key = cv2.waitKey(1)
             if (key == 27):
                 break
-
+                
     def run(self):
-        while True:
-            if self.init:
-                start = time.time()
-                self.inference()
-                self.post()
-                print("Fps:", 1/(time.time() - start))
-
-    def __del__(self):
-        self.cap.release()
-        libc.release_graph_tensor(self.input_tensor)
-        libc.postrun_graph(self.graph)
-        libc.destroy_graph(self.graph)
-        libc.release_tengine()
-        print("Released everything and deleted yolov5")
-
-
-
-if __name__ == "__main__":
-    try:
-        yolov5 = YOLOV5("yolov5m_leaky_352_0mean_uint8.tmfile")
-        yolov5.start()  
-        yolov5.setDaemon(True)  
-        while True:
-            pass
-    except KeyboardInterrupt:
-        del yolov5
-
+        self.inference()
+        
+if __name__ = "__main__":
+  yolov5 = YOLOV5("yolov5m_leaky_352_0mean_uint8.tmfile")
+  yolov5.start()
+  yolov5.setDaemon(True)
+  while True:
+      pass
