ea224ac92f11ce6792c164c98507780282c12975
Alexander <84590713+SashaAlderson@users.noreply.github.com>
2022-05-12 17:44:53 +0700

    Update yolov5.py

--- yolov5.py
+++ yolov5.py
@@ -6,24 +6,27 @@ import copy
 import numpy as np
 import threading
 import multiprocessing as mp
-from multiprocessing import shared_memory
+from multiprocessing import shared_memory, Process
+from post import Img_buffer
+from pre import *
 BUF_SZ = 10
 NUM_PROC = 2
 PROC = 0
-CLASSES = 1
+CLASSES = 80
 NUM_DETS = 300
 #MODEL = "yolov5m_bubbles.tmfile"
-MODEL = "bubbles_3.tmfile"
-#MODEL = "yolov5m_leaky_352_0mean_uint8.tmfile"
+#MODEL = "bubbles_3.tmfile"
+MODEL = "yolov5m_leaky_352_0mean_uint8.tmfile"
 libc = cdll.LoadLibrary("./yolov5_lib.so")
 libc.set_image_wrapper.argtypes = [c_void_p, c_int, c_int, c_void_p, c_int, c_int]
 libc.postpress_graph_image_wrapper.argtypes = [c_int, c_int, c_void_p, c_void_p,
 												c_int , c_int, c_int, c_int, c_int, c_float]
-								
-class YOLOV5(threading.Thread):
-	def __init__(self, model, daemon = True):
-		threading.Thread.__init__(self)
-		self.daemon = daemon	
+
+class YOLOV5():
+	def __init__(self, model, proc):
+		self.proc = proc
+		print("proc: ", self.proc)
+		# self.daemon = daemon	
 		self.img_sz = 352
 		self.context = libc.create_context("timvx".encode('utf-8'), 1)
 		libc.init_tengine()
@@ -41,20 +44,22 @@ class YOLOV5(threading.Thread):
 		# self.draw = True
 		print("Initialised")
 		self.ex_pre = shared_memory.SharedMemory(name="pre") #preprocessed images
-		self.ex_frm= shared_memory.SharedMemory(name="frame") # raw image
+		self.ex_frm = shared_memory.SharedMemory(name="frame") # raw image
 		self.ex_counter = shared_memory.SharedMemory(name="counter") # number of images read from camera
 		self.ex_status = shared_memory.SharedMemory(name="status") # not inferenced images
 		self.ex_read = mp.shared_memory.SharedMemory(name = "read") # number of element to read from
 		self.ex_dets = mp.shared_memory.SharedMemory(name = "dets") 
+		self.ex_change = mp.shared_memory.SharedMemory(name = "change") 
 		self.pre = np.ndarray([BUF_SZ, 3, 352, 352], dtype=np.uint8, buffer=self.ex_pre.buf)
 		self.counter = np.ndarray([1], dtype=np.int64, buffer=self.ex_counter.buf)
 		self.frm =  np.ndarray([BUF_SZ, 480, 640, 3], dtype=np.uint8, buffer=self.ex_frm.buf)
 		self.status = np.ndarray([10], dtype=np.uint8, buffer=self.ex_status.buf)
 		self.read = np.ndarray([NUM_PROC], dtype=np.int64, buffer=self.ex_read.buf)
 		self.dets_buf = np.ndarray([BUF_SZ, NUM_DETS, 6], dtype=np.float32, buffer=self.ex_dets.buf)
+		self.change =  np.ndarray([1], dtype=np.uint8, buffer=self.ex_change.buf)
 
 	def inference(self):
-		if self.status[(self.counter[0]-1)%BUF_SZ] and (self.counter[0]-1)%NUM_PROC == PROC:
+		if self.status[(self.counter[0]-1)%BUF_SZ] and (self.counter[0]-1)%NUM_PROC == self.proc:
 			self.current = (self.counter[0]-1)
 			self.status[self.current%BUF_SZ] = 0 # change status to inferenced
 			start_main = time.time()  
@@ -73,16 +78,64 @@ class YOLOV5(threading.Thread):
 			print("Frame number: ", self.current)
 			print("Full fps: ", 1/(time.time() - start_main))
 			# print("Dets: ", self.last_dets[0])
-			self.read[PROC] = self.current #last inferenced image from 0-9
+			self.read[self.proc] = self.current #last inferenced image from 0-9
 			self.dets_buf[self.current%BUF_SZ][:] = self.dets[:]
 			# print("Write to ", self.current, time.time())
 			# print("Dets buf: ", self.dets_buf)
-
-	def run(self):
-		while True:
-			self.inference()
+		if self.change:
+			print("Changing!")
+			self.ex_pre.close()
+			self.ex_frm.close()
+			self.ex_counter.close()
+			self.ex_status.close()
+			self.ex_read.close()
+			self.ex_dets.close()
+			self.ex_change.close()
+			while True:
+				time.sleep(1)
 			
+def run(proc, model):
+	yolov5 = YOLOV5(model, proc)	
+	while True:
+		yolov5.inference()
 
 if __name__ == "__main__":
-	yolov5 = YOLOV5(MODEL, daemon = False)	
-	yolov5.start()
+	p3 = mp.Process(target=cam, daemon = True)
+	p3.start()
+	# while True:
+	# 	pass
+	p1 = mp.Process(target=run, args = (0, MODEL), daemon = True)
+	p1.start()
+	p2 = mp.Process(target=run, args = (1, MODEL), daemon = True)
+	p2.start()
+	time.sleep(1)
+	ex_stop = mp.shared_memory.SharedMemory(name = "stop") 
+	stop =  np.ndarray([1], dtype=np.uint8, buffer=ex_stop.buf)
+	ex_change = mp.shared_memory.SharedMemory(name = "change") 
+	change =  np.ndarray([1], dtype=np.uint8, buffer=ex_change.buf)
+	while True:
+		command = input()
+		if command == 's':
+			stop[0] = 1
+
+		if command == 'r':
+			stop[0] = 0
+
+		if command == 'change':
+			stop[0] = 0
+			change[0] = 1
+			
+			time.sleep(5)
+			# p3.kill()
+			p1.kill()
+			p2.kill()
+			change[0] = 0
+			
+			# p3 = mp.Process(target=cam, daemon = True)
+			# p3.start()
+			time.sleep(5)
+			p1 = mp.Process(target=run, args = (0, MODEL), daemon = True)
+			p1.start()
+			p2 = mp.Process(target=run, args = (1, MODEL), daemon = True)
+			p2.start()
+			stop[0] = 0
