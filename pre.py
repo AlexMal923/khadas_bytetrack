5fbed8c2da4fb1b1ed26edf085ca122d51e1277d
sashaalderson <alkatolikov@yandex.ru>
2022-05-16 05:09:40 +0000

    change indentation

--- pre.py
+++ pre.py
@@ -17,98 +17,98 @@ NUM_PROC = 2
 NUM_DETS = 300
 
 def letterbox(im, new_shape=(640, 640), color=(114, 114, 114), auto=False, scaleFill=False, stride=32):
-	# Resize and pad image while meeting stride-multiple constraints
-	shape = im.shape[:2]  # current shape [height, width]
-	if isinstance(new_shape, int):
-		new_shape = (new_shape, new_shape)
+    # Resize and pad image while meeting stride-multiple constraints
+    shape = im.shape[:2]  # current shape [height, width]
+    if isinstance(new_shape, int):
+        new_shape = (new_shape, new_shape)
 
-	# Scale ratio (new / old)
-	r = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
-	# if not scaleup:  # only scale down, do not scale up (for better val mAP)
-	#     r = min(r, 1.0)
+    # Scale ratio (new / old)
+    r = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
+    # if not scaleup:  # only scale down, do not scale up (for better val mAP)
+    #     r = min(r, 1.0)
 
-	# Compute padding
-	ratio = r, r  # width, height ratios
-	new_unpad = int(round(shape[1] * r)), int(round(shape[0] * r))
-	dw, dh = new_shape[1] - new_unpad[0], new_shape[0] - new_unpad[1]  # wh padding
-	if auto:  # minimum rectangle
-		dw, dh = np.mod(dw, stride), np.mod(dh, stride)  # wh padding
-	elif scaleFill:  # stretch
-		dw, dh = 0.0, 0.0
-		new_unpad = (new_shape[1], new_shape[0])
-		ratio = new_shape[1] / shape[1], new_shape[0] / shape[0]  # width, height ratios
+    # Compute padding
+    ratio = r, r  # width, height ratios
+    new_unpad = int(round(shape[1] * r)), int(round(shape[0] * r))
+    dw, dh = new_shape[1] - new_unpad[0], new_shape[0] - new_unpad[1]  # wh padding
+    if auto:  # minimum rectangle
+        dw, dh = np.mod(dw, stride), np.mod(dh, stride)  # wh padding
+    elif scaleFill:  # stretch
+        dw, dh = 0.0, 0.0
+        new_unpad = (new_shape[1], new_shape[0])
+        ratio = new_shape[1] / shape[1], new_shape[0] / shape[0]  # width, height ratios
 
-	dw /= 2  # divide padding into 2 sides
-	dh /= 2
-	
-	if shape[::-1] != new_unpad:  # resize
-		im = cv2.resize(im, new_unpad, interpolation=cv2.INTER_LINEAR)
-	top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
-	left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
-	im = cv2.copyMakeBorder(im, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)  # add border
-	im = im[::,::,::-1].transpose((2, 0, 1))
-	return im
+    dw /= 2  # divide padding into 2 sides
+    dh /= 2
+    
+    if shape[::-1] != new_unpad:  # resize
+        im = cv2.resize(im, new_unpad, interpolation=cv2.INTER_LINEAR)
+    top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
+    left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
+    im = cv2.copyMakeBorder(im, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)  # add border
+    im = im[::,::,::-1].transpose((2, 0, 1))
+    return im
 
 
 def cam(source):
-	shm_pre = mp.shared_memory.SharedMemory(create=True, size=BUF_SZ*3*352*352, name = "pre") 
-	shm_frm = mp.shared_memory.SharedMemory(create=True, size=BUF_SZ*3*480*640, name = "frame") 
-	shm_counter = mp.shared_memory.SharedMemory(create=True, size=8, name = "counter") 
-	shm_status = mp.shared_memory.SharedMemory(create=True, size=BUF_SZ, name = "status") 
-	shm_read = mp.shared_memory.SharedMemory(create=True, size=NUM_PROC*8, name = "read") 
-	shm_dets = mp.shared_memory.SharedMemory(create=True, size=BUF_SZ*4*NUM_DETS*6, name = "dets") 
-	shm_stop = mp.shared_memory.SharedMemory(create=True, size=1, name = "stop") 
-	stop =  np.ndarray([1], dtype=np.uint8, buffer=shm_stop.buf)
-	stop[0] = 1 
-	cap = cv2.VideoCapture(source)
-	cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-	cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)	
-	pre = np.ndarray([BUF_SZ, 3, 352, 352], dtype=np.uint8, buffer=shm_pre.buf)
-	frm = np.ndarray([BUF_SZ, 480, 640, 3], dtype=np.uint8, buffer=shm_frm.buf)
-	counter = np.ndarray([1], dtype=np.int64, buffer=shm_counter.buf)
-	status = np.ndarray([10], dtype=np.uint8, buffer=shm_status.buf)
-	counter[0] = 0
-	try:
-		while True:	
-			if not stop:	
-				start_main = time.time()	
-				ret, frame = cap.read()
-				if VID:
-					frame = cv2.resize(frame, [640,480], interpolation = cv2.INTER_LINEAR)
-					print("ONLYFORVIDEO")
-				start = time.time()
-				frm[counter[0]%BUF_SZ][:] = frame 
-				pre[counter[0]%BUF_SZ][:] = letterbox(frame, new_shape=(352, 352))
-				status[counter[0]%BUF_SZ] = 1
-				counter[0] += 1	
-				if VID:
-					slp = 1/FPS -(time.time() - start_main)
-					if slp > 0:
-						time.sleep(slp)
-				print("Fps: ", 1/(time.time() - start_main))
+    shm_pre = mp.shared_memory.SharedMemory(create=True, size=BUF_SZ*3*352*352, name = "pre") 
+    shm_frm = mp.shared_memory.SharedMemory(create=True, size=BUF_SZ*3*480*640, name = "frame") 
+    shm_counter = mp.shared_memory.SharedMemory(create=True, size=8, name = "counter") 
+    shm_status = mp.shared_memory.SharedMemory(create=True, size=BUF_SZ, name = "status") 
+    shm_read = mp.shared_memory.SharedMemory(create=True, size=NUM_PROC*8, name = "read") 
+    shm_dets = mp.shared_memory.SharedMemory(create=True, size=BUF_SZ*4*NUM_DETS*6, name = "dets") 
+    shm_stop = mp.shared_memory.SharedMemory(create=True, size=1, name = "stop") 
+    stop =  np.ndarray([1], dtype=np.uint8, buffer=shm_stop.buf)
+    stop[0] = 1 
+    cap = cv2.VideoCapture(source)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)	
+    pre = np.ndarray([BUF_SZ, 3, 352, 352], dtype=np.uint8, buffer=shm_pre.buf)
+    frm = np.ndarray([BUF_SZ, 480, 640, 3], dtype=np.uint8, buffer=shm_frm.buf)
+    counter = np.ndarray([1], dtype=np.int64, buffer=shm_counter.buf)
+    status = np.ndarray([10], dtype=np.uint8, buffer=shm_status.buf)
+    counter[0] = 0
+    try:
+        while True:	
+            if not stop:	
+                start_main = time.time()	
+                ret, frame = cap.read()
+                if VID:
+                    frame = cv2.resize(frame, [640,480], interpolation = cv2.INTER_LINEAR)
+                    print("ONLYFORVIDEO")
+                start = time.time()
+                frm[counter[0]%BUF_SZ][:] = frame 
+                pre[counter[0]%BUF_SZ][:] = letterbox(frame, new_shape=(352, 352))
+                status[counter[0]%BUF_SZ] = 1
+                counter[0] += 1	
+                if VID:
+                    slp = 1/FPS -(time.time() - start_main)
+                    if slp > 0:
+                        time.sleep(slp)
+                print("Fps: ", 1/(time.time() - start_main))
 
-	finally:
-		print("\nDeleting object")
-		shm_counter.close()
-		shm_counter.unlink()
-		shm_pre.close()
-		shm_pre.unlink()
-		shm_frm.close()
-		shm_frm.unlink()
-		shm_status.close()
-		shm_status.unlink()
+    finally:
+        print("\nDeleting object")
+        shm_counter.close()
+        shm_counter.unlink()
+        shm_pre.close()
+        shm_pre.unlink()
+        shm_frm.close()
+        shm_frm.unlink()
+        shm_status.close()
+        shm_status.unlink()
 
 if __name__ == "__main__":
-	try:
+    try:
 
-		ps = mp.Process(target=cam, args = (), daemon = True)
-		ps.start()
-		time.sleep(0.5)
+        ps = mp.Process(target=cam, args = (), daemon = True)
+        ps.start()
+        time.sleep(0.5)
 
-		while True:
-			time.sleep(0.05)
-	finally:
-		print("\nClosing")
+        while True:
+            time.sleep(0.05)
+    finally:
+        print("\nClosing")
 
 
 
--- yolov5.py
+++ yolov5.py
@@ -18,93 +18,92 @@ MODEL = "yolov5m_leaky_352_0mean_uint8.tmfile"
 libc = cdll.LoadLibrary("./yolov5_lib.so")
 libc.set_image_wrapper.argtypes = [c_void_p, c_int, c_int, c_void_p, c_int, c_int]
 libc.postpress_graph_image_wrapper.argtypes = [c_int, c_int, c_void_p, c_void_p,
-												c_int , c_int, c_int, c_int, c_int, c_float]
+                                                c_int , c_int, c_int, c_int, c_int, c_float]
 
 class YOLOV5():
-	def __init__(self, proc, model):
-		self.proc = proc
-		print("proc: ", self.proc)
-		self.img_sz = 352
-		self.context = libc.create_context("timvx".encode('utf-8'), 1)
-		libc.init_tengine()
-		libc.set_context_device(self.context, "TIMVX".encode('utf-8'), None, 0)
-		model_file = model.encode('utf-8')
-		self.graph = libc.create_graph(self.context, "tengine".encode('utf-8'), model_file)
-		libc.set_graph(352 , self.img_sz , self.graph)
-		self.input_tensor = libc.get_graph_input_tensor(self.graph, 0, 0)
-		self.output_node_num = libc.get_graph_output_node_number(self.graph)
-		self.dets = np.zeros([NUM_DETS,6], dtype = np.float32)  
-		self.classes = libc.get_classes(self.graph)
-		print("Classes: ", self.classes)
-		self.last_dets = None
-		self.nms = 0.2
-		self.current = -1
-		print("Initialised")
-		self.ex_pre = shared_memory.SharedMemory(name="pre") #preprocessed images
-		self.ex_frm = shared_memory.SharedMemory(name="frame") # raw image
-		self.ex_counter = shared_memory.SharedMemory(name="counter") # number of images read from camera
-		self.ex_status = shared_memory.SharedMemory(name="status") # not inferenced images
-		self.ex_read = shared_memory.SharedMemory(name = "read") # number of element to read from
-		self.ex_dets = shared_memory.SharedMemory(name = "dets") # array of detections
-		self.ex_stop = shared_memory.SharedMemory(name = "stop") # array of detections
-		self.pre = np.ndarray([BUF_SZ, 3, 352, 352], dtype=np.uint8, buffer=self.ex_pre.buf)
-		self.counter = np.ndarray([1], dtype=np.int64, buffer=self.ex_counter.buf)
-		self.frm =  np.ndarray([BUF_SZ, 480, 640, 3], dtype=np.uint8, buffer=self.ex_frm.buf)
-		self.status = np.ndarray([10], dtype=np.uint8, buffer=self.ex_status.buf)
-		self.read = np.ndarray([NUM_PROC], dtype=np.int64, buffer=self.ex_read.buf)
-		self.dets_buf = np.ndarray([BUF_SZ, NUM_DETS, 6], dtype=np.float32, buffer=self.ex_dets.buf)
-		self.stop = np.ndarray([1], dtype=np.uint8, buffer=self.ex_stop.buf)
-		self.stop[0] = 0
+    def __init__(self, proc, model):
+        self.proc = proc
+        print("proc: ", self.proc)
+        self.img_sz = 352
+        self.context = libc.create_context("timvx".encode('utf-8'), 1)
+        libc.init_tengine()
+        libc.set_context_device(self.context, "TIMVX".encode('utf-8'), None, 0)
+        model_file = model.encode('utf-8')
+        self.graph = libc.create_graph(self.context, "tengine".encode('utf-8'), model_file)
+        libc.set_graph(352 , self.img_sz , self.graph)
+        self.input_tensor = libc.get_graph_input_tensor(self.graph, 0, 0)
+        self.output_node_num = libc.get_graph_output_node_number(self.graph)
+        self.dets = np.zeros([NUM_DETS,6], dtype = np.float32)  
+        self.classes = libc.get_classes(self.graph)
+        self.last_dets = None
+        self.nms = 0.2
+        self.current = -1
+        print("Initialised")
+        self.ex_pre = shared_memory.SharedMemory(name="pre") #preprocessed images
+        self.ex_frm = shared_memory.SharedMemory(name="frame") # raw image
+        self.ex_counter = shared_memory.SharedMemory(name="counter") # number of images read from camera
+        self.ex_status = shared_memory.SharedMemory(name="status") # not inferenced images
+        self.ex_read = shared_memory.SharedMemory(name = "read") # number of element to read from
+        self.ex_dets = shared_memory.SharedMemory(name = "dets") # array of detections
+        self.ex_stop = shared_memory.SharedMemory(name = "stop") # array of detections
+        self.pre = np.ndarray([BUF_SZ, 3, 352, 352], dtype=np.uint8, buffer=self.ex_pre.buf)
+        self.counter = np.ndarray([1], dtype=np.int64, buffer=self.ex_counter.buf)
+        self.frm =  np.ndarray([BUF_SZ, 480, 640, 3], dtype=np.uint8, buffer=self.ex_frm.buf)
+        self.status = np.ndarray([10], dtype=np.uint8, buffer=self.ex_status.buf)
+        self.read = np.ndarray([NUM_PROC], dtype=np.int64, buffer=self.ex_read.buf)
+        self.dets_buf = np.ndarray([BUF_SZ, NUM_DETS, 6], dtype=np.float32, buffer=self.ex_dets.buf)
+        self.stop = np.ndarray([1], dtype=np.uint8, buffer=self.ex_stop.buf)
+        self.stop[0] = 0
 
-	def inference(self):
-		if self.status[(self.counter[0]-1)%BUF_SZ] and (self.counter[0]-1)%NUM_PROC == self.proc:
-			self.current = (self.counter[0]-1)
-			self.status[self.current%BUF_SZ] = 0 # change status to inferenced
-			start_main = time.time()  
-			self.frame = self.pre[self.current%BUF_SZ]
-			libc.set_image_wrapper(self.frame.ctypes.data, 352, 352, self.input_tensor, self.img_sz, self.img_sz )
-			start = time.time()
-			libc.run_graph(self.graph, 1)
-			print("Graph ran for ", time.time() - start)
-			start = time.time()
-			libc.postpress_graph_image_wrapper(480, 640, self.dets.ctypes.data, 
-												self.graph,self.output_node_num, 352 ,self.img_sz, self.classes, NUM_DETS, self.nms)
-			self.last_dets = copy.deepcopy(self.dets)
-			print("Frame number: ", self.current)
-			print("Full fps: ", 1/(time.time() - start_main))
-			self.read[self.proc] = self.current #last inferenced image from 0-9
-			self.dets_buf[self.current%BUF_SZ][:] = self.dets[:]
+    def inference(self):
+        if self.status[(self.counter[0]-1)%BUF_SZ] and (self.counter[0]-1)%NUM_PROC == self.proc:
+            self.current = (self.counter[0]-1)
+            self.status[self.current%BUF_SZ] = 0 # change status to inferenced
+            start_main = time.time()  
+            self.frame = self.pre[self.current%BUF_SZ]
+            libc.set_image_wrapper(self.frame.ctypes.data, 352, 352, self.input_tensor, self.img_sz, self.img_sz )
+            start = time.time()
+            libc.run_graph(self.graph, 1)
+            print("Graph ran for ", time.time() - start)
+            start = time.time()
+            libc.postpress_graph_image_wrapper(480, 640, self.dets.ctypes.data, 
+                                                self.graph,self.output_node_num, 352 ,self.img_sz, self.classes, NUM_DETS, self.nms)
+            self.last_dets = copy.deepcopy(self.dets)
+            print("Frame number: ", self.current)
+            print("Full fps: ", 1/(time.time() - start_main))
+            self.read[self.proc] = self.current #last inferenced image from 0-9
+            self.dets_buf[self.current%BUF_SZ][:] = self.dets[:]
 
-			
+            
 def run(proc, model):
-	yolov5 = YOLOV5(proc, model)	
-	while True:
-		yolov5.inference()
+    yolov5 = YOLOV5(proc, model)	
+    while True:
+        yolov5.inference()
 
 if __name__ == "__main__":
-	p1 = Process(target=cam, args = (SOURCE,))
-	p1.start()
-	p2 = Process(target=run, args = (0, MODEL))
-	p2.start()
-	p3 = Process(target=run, args = (1, MODEL))
-	p3.start()
-	time.sleep(1)
-	ex_stop = shared_memory.SharedMemory(name = "stop") 
-	stop =  np.ndarray([1], dtype=np.uint8, buffer=ex_stop.buf)
-	while True:
-		command = input()
-		if command == 's':
-			stop[0] = 1
+    p1 = Process(target=cam, args = (SOURCE,))
+    p1.start()
+    p2 = Process(target=run, args = (0, MODEL))
+    p2.start()
+    p3 = Process(target=run, args = (1, MODEL))
+    p3.start()
+    time.sleep(1)
+    ex_stop = shared_memory.SharedMemory(name = "stop") 
+    stop =  np.ndarray([1], dtype=np.uint8, buffer=ex_stop.buf)
+    while True:
+        command = input()
+        if command == 's':
+            stop[0] = 1
 
-		if command == 'r':
-			stop[0] = 0
+        if command == 'r':
+            stop[0] = 0
 
-		if command == 'c':
-			stop[0] = 1
-			p2.kill()
-			p3.kill()
-			p2 = Process(target=run, args = (0, MODEL), daemon = True)
-			p2.start()
-			p3 = Process(target=run, args = (1, MODEL), daemon = True)
-			p3.start()
-			
+        if command == 'c':
+            stop[0] = 1
+            p2.kill()
+            p3.kill()
+            p2 = Process(target=run, args = (0, MODEL), daemon = True)
+            p2.start()
+            p3 = Process(target=run, args = (1, MODEL), daemon = True)
+            p3.start()
+            
